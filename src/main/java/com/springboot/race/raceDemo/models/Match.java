a7272fbaa535b600b8779b9151b42715e9205625
GeorgeXenidis <33318467+GeorgeXenidis@users.noreply.github.com>
2021-12-04 14:34:21 +0200

    Declared Entities and Relations Between them. Closed issues #2  and #5

--- src/main/java/com/springboot/race/raceDemo/models/Match.java
+++ src/main/java/com/springboot/race/raceDemo/models/Match.java
@@ -2,6 +2,7 @@ package com.springboot.race.raceDemo.models;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -10,9 +11,17 @@ import java.time.LocalTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Match {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //Relations Below
+    @OneToOne(mappedBy = "match",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private MatchOdds matchOdds;
+
     private String description;
     private LocalDate matchDate;
     private LocalTime matchTime;
--- src/main/java/com/springboot/race/raceDemo/models/MatchOdds.java
+++ src/main/java/com/springboot/race/raceDemo/models/MatchOdds.java
@@ -2,14 +2,24 @@ package com.springboot.race.raceDemo.models;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Setter
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class MatchOdds {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    //Relations Below
+    @OneToOne
+    private Match match;
+
     private String specifier;
     private Double odd;
 
