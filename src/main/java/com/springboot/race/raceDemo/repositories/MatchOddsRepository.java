be543bacf567e891655d3abdb763e5c1e2d0abb2
GeorgeXenidis <33318467+GeorgeXenidis@users.noreply.github.com>
2021-12-04 14:38:52 +0200

    Added Necessary Annotations

--- src/main/java/com/springboot/race/raceDemo/repositories/MatchOddsRepository.java
+++ src/main/java/com/springboot/race/raceDemo/repositories/MatchOddsRepository.java
@@ -2,6 +2,8 @@ package com.springboot.race.raceDemo.repositories;
 
 import com.springboot.race.raceDemo.models.MatchOdds;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MatchOddsRepository extends JpaRepository<MatchOdds, Long> {
 }
--- src/main/java/com/springboot/race/raceDemo/repositories/MatchRepository.java
+++ src/main/java/com/springboot/race/raceDemo/repositories/MatchRepository.java
@@ -2,6 +2,8 @@ package com.springboot.race.raceDemo.repositories;
 
 import com.springboot.race.raceDemo.models.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
 }
