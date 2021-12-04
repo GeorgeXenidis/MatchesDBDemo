fc977c51a12fd3ba92e7889bab2d640913965132
GeorgeXenidis <33318467+GeorgeXenidis@users.noreply.github.com>
2021-12-04 17:19:59 +0200

    Completed a Get-Request

--- src/main/java/com/springboot/race/raceDemo/controllers/SingleController.java
+++ src/main/java/com/springboot/race/raceDemo/controllers/SingleController.java
@@ -1,12 +1,15 @@
 package com.springboot.race.raceDemo.controllers;
 
+import com.springboot.race.raceDemo.dtos.MatchDetailsDto;
+import com.springboot.race.raceDemo.models.Match;
 import com.springboot.race.raceDemo.repositories.MatchOddsRepository;
 import com.springboot.race.raceDemo.repositories.MatchRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import org.modelmapper.ModelMapper;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequestMapping("/match")
 @Controller
@@ -14,15 +17,28 @@ public class SingleController {
 
     private final MatchRepository matchRepository;
     private final MatchOddsRepository matchOddsRepository;
+    private ModelMapper modelMapper;
 
-    public SingleController(MatchRepository matchRepository, MatchOddsRepository matchOddsRepository) {
+    public SingleController(MatchRepository matchRepository, MatchOddsRepository matchOddsRepository, ModelMapper modelMapper) {
         this.matchRepository = matchRepository;
         this.matchOddsRepository = matchOddsRepository;
+        this.modelMapper = modelMapper;
     }
 
-    @GetMapping("/getMatchDetails")
-    public void getMatchDetails(){
-        
+    @GetMapping("/getMatchDetails/{id}")
+    @ResponseBody
+    public MatchDetailsDto getMatchDetails(@PathVariable Long id){
+
+        Optional<Match> optionalMatch = matchRepository.findById(id);
+
+        if(optionalMatch.isEmpty()){
+            throw  new NoSuchElementException("The match with the given ID couldn't be found");
+        }
+
+        Match match = optionalMatch.get();
+        MatchDetailsDto matchDetailsDto = new MatchDetailsDto(match);
+
+        return matchDetailsDto;
     }
 
     @PostMapping("/createMatch")
